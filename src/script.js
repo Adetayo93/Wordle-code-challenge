c1dfb0ec29dbab501ec2e26c9986f3fab8ffccce
Adetayo93 <tayo_123@hotmail.co.uk>
2024-04-30 22:47:12 +0100

    input field

---
+++ src/script.js
@@ -0,0 +1,7 @@
+function submitGuess() {
+    var guessInput = document.getElementById('guess-input').value;
+    
+    // Perform validation or further processing of the guess input
+    // For now, let's just log the guess to the console
+    console.log('Guess:', guessInput);
+}
